790875e60b2f52edb17cdd429fb77168310174a0
Chris Choi <chutsu@gmail.com>
2013-08-21 08:00:48 +0100

    Update Travis and install dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-mkdir build_dep
+mkdir build
 
-cd build_dep
+cd build
 git clone https://github.com/chutsu/munit.git
 git clone https://github.com/chutsu/dbg.git
 git clone https://github.com/chutsu/al.git
 git clone https://github.com/chutsu/dstruct.git
 
 cd munit
-sudo make && sudo make install
+make && make install
 cd ..
 
 cd dbg
-cmake . && make && sudo make install
+cmake . && make && make install
 cd ..
 
 cd al
-cmake . && make && sudo make install
+cmake . && make && make install
 cd ..
 
 cd dstruct
-cmake . && make && sudo make install
+cmake . && make && make install
 cd ..
 
-rm -rf build_dep
+rm -rf build
