e7e0a583037bb4bdb4b563fd30054b9073e1bc67
Chris Choi <chutsu@gmail.com>
2013-09-03 23:19:52 +0100

    Update travis

--- scripts/unittest_runner.py
+++ scripts/unittest_runner.py
@@ -2,12 +2,13 @@
 import os
 import re
 import sys
+import shutil
 import subprocess
 
 
 # SETTINGS
 keep_unittest_logs = False
-unittests_bin_dir = "../bin"
+unittests_bin_dir = "bin"
 unittests_log_dir = "unittests_log"
 unittests_file_pattern = "^[a-zA-Z0-9_]*_tests$"
 
@@ -57,12 +58,24 @@ if __name__ == "__main__":
 
         # print result
         if return_val != 0:
-            print("{0}FAILED!{1}".format(TermColors.FAIL, TermColors.ENDC))
             error = True
+            print("{0}FAILED!{1}".format(TermColors.FAIL, TermColors.ENDC))
+
+            # open unittest stdout log file
+            unittest_output = open(unittest_output_fp, 'w')
+            output_content = unittest_output.read()
+            unittest_output.close()
+
+            # print unittest stdout
+            print("-" * 79)
+            print(output_content)
+            print("-" * 79)
         else:
             print("{0}PASSED!{1}".format(TermColors.OKGREEN, TermColors.ENDC))
-            if keep_unittest_logs is False:
-                os.remove(unittest_output_fp)
+
+    # remove unittest stdout dir
+    os.chdir(orig_cwd)
+    shutil.rmtree(unittests_log_dir)
 
     if error is True:
         sys.exit(-1)
